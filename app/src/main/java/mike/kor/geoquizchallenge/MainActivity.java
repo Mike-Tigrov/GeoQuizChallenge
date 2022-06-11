88129006718a961fbd78f6792581cebb82c761d7
Mike-Tigrov <xiwnik@yahoo.com>
2022-06-11 20:42:18 +0300

    Some new code ;)

--- app/src/main/java/mike/kor/geoquizchallenge/MainActivity.java
+++ app/src/main/java/mike/kor/geoquizchallenge/MainActivity.java
@@ -6,17 +6,37 @@ import android.view.View;
 import android.widget.Button;
 
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+
+    //Создаём массив объектов
+    private Question[] mQuestionBank = new Question[] {
+            new Question(R.string.question_australia, true),
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true),
+    };
+//Переменная для индекса массива
+    private int mCurrentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
 
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
